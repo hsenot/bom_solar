d5f6d195b14f0735f9e40b4bfd8cfb2dcc39ae09
Herve Senot <herve.senot@groundtruth.com.au>
2013-12-05 12:27:41 +1100

    Adding basic usage instructions

--- README.md
+++ README.md
@@ -16,3 +16,21 @@ mkvirtualenv bom_solar
 pip install psycopg2
 
 
+Use
+---
+* Load BOM solar data as vector
+
+Adjust DB connection details in txt2pg.py
+python txt2pg.py -d <source_directory_of_raster_files> -x <lon> -y <lat>
+
+* Load BOM solar data as raster
+
+1 raster at a time (1 file = 1 record):
+raster2pgsql -I -C -s 4283 <raster_file> | sudo -u postgres psql -d <database_name>
+
+
+
+
+
+
+
--- txt2pg.py
+++ txt2pg.py
@@ -97,7 +97,7 @@ def main(argv):
 
 	sql = """
 create table bom_analysis as
-select max(id) as id,
+select max(id) as id,pixel_ref,
 (case when (ut_time in ('00','01','02','03','04','05','06','07','08','09','10','11'))
 then ut_date
 else to_char(to_date(ut_date,'YYYYMMDD')+1,'YYYYMMDD')
@@ -108,14 +108,14 @@ val
 from
 (
 select *,
+floor(x)||'x'||floor(y) as pixel_ref,
 substring(fn,position('dni_' in fn)+4,8) as ut_date,
 substring(fn,position('dni_' in fn)+13,2) as ut_time
- from bom_pixel 
-order by fn
+ from bom_pixel
 ) t
 where val <> -999
-group by local_date,local_obs_hr_nb,val
-order by local_date,local_obs_hr_nb
+group by pixel_ref,local_date,local_obs_hr_nb,val
+order by pixel_ref,local_date,local_obs_hr_nb
 """
 	print sql
 	cur.execute(sql)
@@ -127,7 +127,7 @@ order by local_date,local_obs_hr_nb
 	cur.execute(sql)
 	conn.commit()
 
-	sql = "CREATE INDEX bom_analysis_date_idx ON bom_analysis USING btree (local_date)"
+	sql = "CREATE INDEX bom_analysis_pixel_ref_date_idx ON bom_analysis USING btree (pixel_ref,local_date)"
 	print sql
 	cur.execute(sql)
 	conn.commit()
